e13126c17bc54a090f3891f3e1cde2d8791025f8
klayverxd <klayverx@gmail.com>
2021-07-16 09:38:33 -0300

    mobile version

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const userLogin = document.getElementById('login')
 const public_repos = document.getElementById('public_repos')
 const followers = document.getElementById('followers')
 const following = document.getElementById('following')
+const avatar_url = document.getElementById('avatar')
 
 function getUserData() {
 	fetch('https://api.github.com/users/klayverxd')
 		.then(response => response.json())
 		.then(data => {
+			avatar_url.src = data.avatar_url
 			userName.innerHTML = data.name
 			userLogin.innerHTML = '@' + data.login
 			public_repos.innerHTML = data.public_repos
